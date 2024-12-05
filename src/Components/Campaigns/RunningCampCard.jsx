04f55088aca7291d34fea71f382cf88cc4032d55
ahanaf607307 <ahanaf607307@gmail.com>
2024-12-06 03:07:41 +0600

    card design compleate

--- src/Components/Campaigns/RunningCampCard.jsx
+++ src/Components/Campaigns/RunningCampCard.jsx
@@ -4,24 +4,27 @@ import { Link } from 'react-router-dom'
 function RunningCampCard({camps}) {
   const {_id,imageUrl,campignsName,campignsType,minDonation,deadline,userEmail,userName} = camps
   return (
-    <div className="card flex flex-col h-[450px] card-compact bg-base-100  shadow-xl animate__jackInTheBox animate__animated">
+    <div className="card flex flex-col h-[450px] card-compact bg-base-100  shadow-xl animate__jackInTheBox animate__animated relative ">
   <div className=''>
     <img
       src={imageUrl}
       alt="campaigns"
-      className='h-48 md:h-56 w-full rounded-t-xl'/>
+      className='h-48 md:h-60 w-full rounded-t-xl'/>
   </div>
-  <div className="my-3 px-2 flex-1 space-y-2">
+  <div className="my-3 px-2 flex-1 space-y-2 text-center">
     
-    <h1 className='text-2xl font-bold'>{campignsName}</h1>
-    <h2>Tpye : {campignsType}</h2>
+    <h1 className='text-2xl my-4 font-bold'>{campignsName}</h1>
+    <h2 className='absolute top-52 right-24 text-white rounded-sm bg-orange-600 px-2 py-1'> {campignsType}</h2>
  
   
-    <h1>Deadline : {deadline}</h1>
+  <div className='flex justify-between text-gray-500 px-3 mt-5'>
+  <h1>Deadline : {deadline}</h1>
+  <h1>{minDonation}</h1>
+  </div>
   
 
   </div>
-  <Link to={`/details/${_id}`} className='btn w-full bg-orange-600 text-white'>See more </Link>
+  <Link to={`/details/${_id}`} className='btn w-full bg-blue-200 '>See more </Link>
 </div>
   )
 }
--- src/Components/Campaigns/RunningCampaigns.jsx
+++ src/Components/Campaigns/RunningCampaigns.jsx
@@ -10,9 +10,10 @@ const {camp} = useContext(CampApi)
 console.log(camp)
   return (
     <div>
-      <h1 className='text-3xl text-center font-bold mt-10'>Running Campaign</h1>
-      <h1 className='text-xl text-center font-bold mt-5 mb-10'>Meet people and life-changing teachers in need of your support</h1>
-<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 '>
+      <h1 className='text-sm text-orange-600 text-center font-bold mt-4'>-- BUSINESSES YOU CAN BACK --</h1>
+      <h1 className='text-2xl md:text-4xl lg:px-60 px-5 md:px-36 text-center font-bold mt-5 mb-10'>Best Featured Projects
+      Explore Now</h1>
+<div className='grid p-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 '>
   {
     camp.map(camps=><RunningCampCard key={camps?._id} camps={camps} /> )
   }
