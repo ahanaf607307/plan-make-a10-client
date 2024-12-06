e1b91ee0ecc814a97a281ade6019ae848c03ec16
ahanaf607307 <ahanaf607307@gmail.com>
2024-12-06 18:02:07 +0600

    compleate banner section

--- src/Main/Home.jsx
+++ src/Main/Home.jsx
@@ -10,7 +10,7 @@ const runningCampData = useLoaderData()
 console.log(runningCampData)
 
   return (
-    <div className='my-10'>
+    <div className='my-12'>
       <div>
         <Banner/>
       </div>
