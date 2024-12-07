99dc649a2a4f6216a5b2d787e5fef61d9b5c6965
ahanaf607307 <ahanaf607307@gmail.com>
2024-12-07 22:55:50 +0600

    fix login error issue and remove some comments line

--- src/Main/MainLayout.jsx
+++ src/Main/MainLayout.jsx
@@ -1,27 +1,4 @@
-// import React from 'react'
-// import { Outlet } from 'react-router-dom'
-// import Footer from '../Components/Footer/Footer'
-// import Navbar from '../Components/Navbar/Navbar'
 
-// function MainLayout() {
-//   return (
-
-//     <div className='md:w-[800px] lg:w-[1024px] h-screen mx-auto flex flex-col'>
-//         <section>
-//             <Navbar/>
-//         </section>
-//       <section className='flex-1'>
-//       <Outlet/>
-//       </section>
-//        <section>
-//         <Footer/>
-//       </section>
-//     </div>
-
-//   )
-// }
-
-// export default MainLayout
 
 import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
